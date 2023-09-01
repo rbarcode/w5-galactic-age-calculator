6f09eae6047afa9780308321f92261f1044e96cd
Richard Barbour <richyb07@gmail.com>
2023-09-01 13:37:22 -0700

    Create new branch to test using a separate class for each planet, write age-calc test 1 and complete red phase.

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -7,18 +7,18 @@ describe('AgeCaculator', () => {
     ageCalc = new AgeCalculator(20);
   });
 
-  test('should create a agecalculator object and calculate current galactic ages', () => {
-    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: "0.00", pastMercuryYears: "0.00", pastVenusYears: "0.00", pastMarsYears: "0.00", pastJupiterYears: "0.00", futureEarthYears: "0.00", futureMercuryYears: "0.00", futureVenusYears: "0.00", futureMarsYears: "0.00", futureJupiterYears: "0.00"});
+  test('should create a agecalculator object for earth age', () => {
+    expect(ageCalc).toEqual({ earthAge: 20, pastEarthYears: "0.00", futureEarthYears: "0.00"});
   });
 
-  test('should calculate how many years have passed on each planet since a past birthday', () => {
-    ageCalc.calcYearsSinceBday(17);
-    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: "3.00", pastMercuryYears: "12.50", pastVenusYears: "4.84", pastMarsYears: "1.60", pastJupiterYears: "0.25", futureEarthYears: "0.00", futureMercuryYears: "0.00", futureVenusYears: "0.00", futureMarsYears: "0.00", futureJupiterYears: "0.00"});
-  });
+  // test('should calculate how many years have passed on earth since a past birthday', () => {
+  //   ageCalc.calcYearsSinceBday(17);
+  //   expect(ageCalc).toEqual({ earthAge: 20, pastEarthYears: "3.00", futureEarthYears: "0.00"});
+  // });
 
-  test('should calculate how many years will pass on each planet until a given future birthday', () => {
-    ageCalc.calcYearsToBirthday(32);
-    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: "0.00", pastMercuryYears: "0.00", pastVenusYears: "0.00", pastMarsYears: "0.00", pastJupiterYears: "0.00", futureEarthYears: "12.00", futureMercuryYears: "50.00", futureVenusYears: "19.35", futureMarsYears: "6.38", futureJupiterYears: "1.01"});
-  });
+  // test('should calculate how many years will pass on earth until a given future birthday', () => {
+  //   ageCalc.calcYearsToBirthday(32);
+  //   expect(ageCalc).toEqual({ earthAge: 20, pastEarthYears: "0.00", futureEarthYears: "12.00"});
+  // });
 
 });
--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -1,42 +1,18 @@
 export class AgeCalculator {
   constructor(age) {
-    this.earthAge = age;
-    this.earthYear = 1;
-    this.mercuryYear = .24;
-    this.venusYear = .62;
-    this.marsYear = 1.88;
-    this.jupiterYear = 11.86;
-    this.mercuryAge = (this.earthAge / this.mercuryYear).toFixed(2);
-    this.venusAge = (this.earthAge / this.venusYear).toFixed(2);
-    this.marsAge = (this.earthAge / this.marsYear).toFixed(2);
-    this.jupiterAge = (this.earthAge / this.jupiterYear).toFixed(2);
-    this.pastEarthYears = (0).toFixed(2);
-    this.pastMercuryYears = (0).toFixed(2);
-    this.pastVenusYears = (0).toFixed(2);
-    this.pastMarsYears = (0).toFixed(2);
-    this.pastJupiterYears = (0).toFixed(2);
-    this.futureEarthYears = (0).toFixed(2);
-    this.futureMercuryYears = (0).toFixed(2);
-    this.futureVenusYears = (0).toFixed(2);
-    this.futureMarsYears = (0).toFixed(2);
-    this.futureJupiterYears = (0).toFixed(2);
+    // this.earthAge = age;
+    // this.pastEarthYears = (0).toFixed(2);
+    // this.futureEarthYears = (0).toFixed(2);
+  
   }
 
-  calcYearsSinceBday(previousBday) {
-    this.pastEarthYears = (this.earthAge - previousBday).toFixed(2);
-    this.pastMercuryYears = (this.pastEarthYears / this.mercuryYear).toFixed(2);
-    this.pastVenusYears = (this.pastEarthYears / this.venusYear).toFixed(2);
-    this.pastMarsYears = (this.pastEarthYears / this.marsYear).toFixed(2);
-    this.pastJupiterYears = (this.pastEarthYears / this.jupiterYear).toFixed(2);
-  }
+  // calcYearsSinceBday(previousBday) {
+  //   this.pastEarthYears = (this.earthAge - previousBday).toFixed(2);
+  // }
 
-  calcYearsToBirthday(futureBday) {
-    this.futureEarthYears = (futureBday - this.earthAge).toFixed(2);
-    this.futureMercuryYears = (this.futureEarthYears / this.mercuryYear).toFixed(2);
-    this.futureVenusYears = (this.futureEarthYears / this.venusYear).toFixed(2);
-    this.futureMarsYears = (this.futureEarthYears / this.marsYear).toFixed(2);
-    this.futureJupiterYears = (this.futureEarthYears / this.jupiterYear).toFixed(2);
-  }
+  // calcYearsToBirthday(futureBday) {
+  //   this.futureEarthYears = (futureBday - this.earthAge).toFixed(2);
+  // }
 
 
 }
