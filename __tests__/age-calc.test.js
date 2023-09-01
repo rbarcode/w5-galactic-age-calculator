915d78508b0a95dbfa3d20383ffba4278120d634
Richard Barbour <richyb07@gmail.com>
2023-09-01 12:01:13 -0700

    Write age-calc test 3 and complete red phase.

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -16,4 +16,9 @@ describe('AgeCaculator', () => {
     expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: "3.00", pastMercuryYears: "12.50", pastVenusYears: "4.84", pastMarsYears: "1.60", pastJupiterYears: "0.25", futureEarthYears: "0.00", futureMercuryYears: "0.00", futureVenusYears: "0.00", futureMarsYears: "0.00", futureJupiterYears: "0.00"});
   });
 
+  test('should calculate how many years will pass on each planet until a given future birthday', () => {
+    ageCalc.calcYearsToBirthday(32);
+    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: "0.00", pastMercuryYears: "0.00", pastVenusYears: "0.00", pastMarsYears: "0.00", pastJupiterYears: "0.00", futureEarthYears: "12.00", futureMercuryYears: "50.00", futureVenusYears: "19.35", futureMarsYears: "6.38", futureJupiterYears: "1.01"});
+  });
+
 });
--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -30,5 +30,9 @@ export class AgeCalculator {
     this.pastJupiterYears = (this.pastEarthYears / this.jupiterYear).toFixed(2);
   }
 
+  calcYearsToBirthday(futureBday) {
+    
+  }
+
 
 }
