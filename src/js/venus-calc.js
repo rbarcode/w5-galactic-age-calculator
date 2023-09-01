e5181574106aae90d885751a1689ff595512933a
Richard Barbour <richyb07@gmail.com>
2023-09-01 14:41:10 -0700

    Pass venus-calc test 1 and complete green phase.

--- src/js/venus-calc.js
+++ src/js/venus-calc.js
@@ -1,7 +1,11 @@
 export class VenusCalc {
 
   constructor(earthAge) {
-
+    this.earthAge = earthAge;
+    this.venusYear = .62;
+    this.venusAge = (this.earthAge / this.venusYear).toFixed(2);
+    this.pastVenusYears = (0).toFixed(2);
+    this.futureVenusYears = (0).toFixed(2);
   }
 
 
