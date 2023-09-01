f20564a14e766e92dd2ae2c3e5a803b882472ee6
Richard Barbour <richyb07@gmail.com>
2023-09-01 09:08:35 -0700

    Refactor age-calc test 1 and complete red phase.

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -8,8 +8,7 @@ describe('AgeCaculator', () => {
   });
 
   test('should create a agecalculator object', () => {
-    expect(ageCalc).toEqual({ age: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86 });
+    expect(ageCalc).toEqual({ earthAge: 20, mercuryAge: undefined, venusAge: undefined,marsAge: undefined, jupiterAge: undefined, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86 });
   });
-
 
 });
--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -1,10 +1,10 @@
 export class AgeCaculator {
   constructor(age) {
-    this.age = age;
-		this.earthYear = 1;
-		this.mercuryYear = .24;
-		this.venusYear = .62;
-		this.marsYear = 1.88;
-		this.jupiterYear = 11.86;
+    // this.age = age;
+		// this.earthYear = 1;
+		// this.mercuryYear = .24;
+		// this.venusYear = .62;
+		// this.marsYear = 1.88;
+		// this.jupiterYear = 11.86;
   }
 }
