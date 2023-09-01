360dfbaa46dfcc3e3c27330ddd3f1ce32376b218
Richard Barbour <richyb07@gmail.com>
2023-09-01 10:11:32 -0700

    Refactor and pass (yet again) age-calc test 1 and complete green phase, delete original age-calc test 2.

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -7,12 +7,8 @@ describe('AgeCaculator', () => {
     ageCalc = new AgeCaculator(20);
   });
 
-  test('should create a agecalculator object', () => {
-    expect(ageCalc).toEqual({ earthAge: 20, mercuryAge: undefined, venusAge: undefined, marsAge: undefined, jupiterAge: undefined, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86 });
+  test('should create a agecalculator object with current galactic ages', () => {
+    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69" });
   });
-
-  // test('should calculate current earth age in galactic years', () => {
-  //   expect(ageCalc).toEqual({ earthAge: 20, mercuryAge: 83.33, venusAge: 32.26, marsAge: 10.64, jupiterAge: 1.69, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86 });
-  // })
 
 });
--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -1,19 +1,16 @@
 export class AgeCaculator {
   constructor(age) {
     this.earthAge = age;
-    this.mercuryAge;
-		this.venusAge;
-		this.marsAge;
-		this.jupiterAge;
-		this.earthYear = 1;
+    this.earthYear = 1;
 		this.mercuryYear = .24;
 		this.venusYear = .62;
 		this.marsYear = 1.88;
 		this.jupiterYear = 11.86;
+    this.mercuryAge = (this.earthAge / this.mercuryYear).toFixed(2);
+		this.venusAge = (this.earthAge / this.venusYear).toFixed(2);
+		this.marsAge = (this.earthAge / this.marsYear).toFixed(2);
+		this.jupiterAge = (this.earthAge / this.jupiterYear).toFixed(2);
+
   }
-
-  // calcCurrentAge() {
-
-  // }
 
 }
