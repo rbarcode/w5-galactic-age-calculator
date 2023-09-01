e0cf80a2844c2bc8c27e9cdfb47a99811835273e
Richard Barbour <richyb07@gmail.com>
2023-09-01 11:15:15 -0700

    Refactor and pass (yes, and yet again) age-calc test 1 and complete green phase.

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -8,7 +8,8 @@ describe('AgeCaculator', () => {
   });
 
   test('should create a agecalculator object', () => {
-    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: 0, pastMercuryYears: 0, pastVenusYears: 0, pastMarsYears: 0, pastJupiterYears: 0, futureEarthYears: 0, futureMercuryYears: 0, futureVenusYears: 0, futureMarsYears: 0, futureJupiterYears: 0});
+    expect(ageCalc).toEqual({ earthAge: 20, earthYear: 1, mercuryYear: .24, venusYear: .62, marsYear: 1.88, jupiterYear: 11.86, mercuryAge: "83.33", venusAge: "32.26", marsAge: "10.64", jupiterAge: "1.69", pastEarthYears: "0.00", pastMercuryYears: "0.00", pastVenusYears: "0.00", pastMarsYears: "0.00", pastJupiterYears: "0.00", futureEarthYears: "0.00", futureMercuryYears: "0.00", futureVenusYears: "0.00", futureMarsYears: "0.00", futureJupiterYears: "0.00"});
   });
+
 
 });
--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -10,16 +10,17 @@ export class AgeCalculator {
     this.venusAge = (this.earthAge / this.venusYear).toFixed(2);
     this.marsAge = (this.earthAge / this.marsYear).toFixed(2);
     this.jupiterAge = (this.earthAge / this.jupiterYear).toFixed(2);
-    this.pastEarthYears = 0;
-    this.pastMercuryYears = 0;
-    this.pastVenusYears = 0;
-    this.pastMarsYears = 0;
-    this.pastJupiterYears = 0;
-    this.futureEarthYears = 0;
-    this.futureMercuryYears = 0;
-    this.futureVenusYears = 0;
-    this.futureMarsYears = 0;
-    this.futureJupiterYears = 0;
+    this.pastEarthYears = (0).toFixed(2);
+    this.pastMercuryYears = (0).toFixed(2);
+    this.pastVenusYears = (0).toFixed(2);
+    this.pastMarsYears = (0).toFixed(2);
+    this.pastJupiterYears = (0).toFixed(2);
+    this.futureEarthYears = (0).toFixed(2);
+    this.futureMercuryYears = (0).toFixed(2);
+    this.futureVenusYears = (0).toFixed(2);
+    this.futureMarsYears = (0).toFixed(2);
+    this.futureJupiterYears = (0).toFixed(2);
   }
+
 
 }
