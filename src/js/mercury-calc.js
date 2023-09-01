18dafba773b77fe2b29b19f6b0651b2acc2922ef
Richard Barbour <richyb07@gmail.com>
2023-09-01 13:55:44 -0700

    Pass mercury-calc test 1 and complete green phase.

--- src/js/mercury-calc.js
+++ src/js/mercury-calc.js
@@ -2,6 +2,10 @@
 export class MercuryCalc {
 
   constructor(earthAge) {
-
+    this.earthAge = earthAge;
+    this.mercuryYear = .24;
+    this.mercuryAge = (this.earthAge / this.mercuryYear).toFixed(2);
+    this.pastMercuryYears = (0).toFixed(2);
+    this.futureMercuryYears = (0).toFixed(2);
   }
 }
