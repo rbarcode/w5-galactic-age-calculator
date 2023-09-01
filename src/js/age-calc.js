f5c4cf63b986d79de7599660dd29867cc6ca094f
Richard Barbour <richyb07@gmail.com>
2023-09-01 09:11:21 -0700

    Pass refactored age-calc test 1 and complete green phase.

--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -1,10 +1,14 @@
 export class AgeCaculator {
   constructor(age) {
-    // this.age = age;
-		// this.earthYear = 1;
-		// this.mercuryYear = .24;
-		// this.venusYear = .62;
-		// this.marsYear = 1.88;
-		// this.jupiterYear = 11.86;
+    this.earthAge = age;
+    this.mercuryAge = undefined;
+		this.venusAge = undefined;
+		this.marsAge = undefined;
+		this.jupiterAge = undefined;
+		this.earthYear = 1;
+		this.mercuryYear = .24;
+		this.venusYear = .62;
+		this.marsYear = 1.88;
+		this.jupiterYear = 11.86;
   }
 }
