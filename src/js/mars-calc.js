c858afef858b45ff3dcb2f45db0dbd3e83690712
Richard Barbour <richyb07@gmail.com>
2023-09-01 15:02:55 -0700

    Pass mars-calc test 1 and complete green phase.

--- src/js/mars-calc.js
+++ src/js/mars-calc.js
@@ -1,7 +1,11 @@
 export class MarsCalc {
 
   constructor(earthAge) {
-
+    this.earthAge = earthAge;
+    this.marsYear = 1.88;
+    this.marsAge = (this.earthAge / this.marsYear).toFixed(2);
+    this.pastMarsYears = (0).toFixed(2);
+    this.futureMarsYears = (0).toFixed(2);
   }
 
 }
